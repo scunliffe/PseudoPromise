251035be689863f66e73fe436dc24508ae952e2f
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2014-01-04 00:33:09 -0500

    Update usage.js
    some additional options/info added. cleanup and refactoring required.

--- usage.js
+++ usage.js
@@ -1,4 +1,4 @@
-//basic usage
+//Basic usage:
 
 //initialize...
 var swear = new PseudoPromise();
@@ -10,3 +10,35 @@ swear.addMethod('baz', doBaz);
 
 //start the batch of functions...
 swear.start(3, mySuccessHandler, myErrorHandler);
+
+
+//=============================================================
+
+//Other Methods/Options:
+
+//Add functions...
+swear.addMethod(functionID, functionReference);
+
+
+//indicate when a asynchronous function has completed (success or error)
+swear.markComplete(functionID, wasSuccessful);
+
+
+//start the batch of functions...
+swear.start(minCompletedCount, successHandler, errorHandler);
+
+//setting the minCompletedCount to 1 will execute the successHandler when the first (e.g. any) function completes successfully
+
+/*
+  Notes:
+    - If any completed function is marked as an error the overall errorHandler is called (and further handling of
+      completed functions is ignored)
+    - Currently each method added requires an ID... but this might change
+    - In most typical cases the desired behavior is an AND condtion where all the functions should complete
+      before calling the overall success handler... however to obtain an OR condition behavior setting the
+      minCompletedCount to 1 will achieve this
+*/
+
+
+
+
